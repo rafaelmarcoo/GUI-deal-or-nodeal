556b2e61a428f49c8f335b48c8b1cc147e2b87e2
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-09-16 19:32:06 +1200

    method for DB Game Log

---
+++ src/com/mycompany/deal/or/nodeal/DBGameLog.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.deal.or.nodeal;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author rafae
+ */
+public class DBGameLog 
+{
+    public static void dbGameLog(String firstName, String lastName, String action)
+    {
+        LocalDateTime time = LocalDateTime.now();
+        Timestamp timeStamp = Timestamp.valueOf(time);
+        
+        String dbURL = "jdbc:derby:dealornodealDB;create=true";
+        
+        try
+        {
+            Connection conn = DriverManager.getConnection(dbURL);
+            Statement stmt = conn.createStatement();
+            
+            DatabaseMetaData dbMeta = conn.getMetaData();
+            ResultSet rs = dbMeta.getTables(null, null, firstName + "_" + lastName, null);
+            if(!rs.next())
+            {
+                stmt.executeUpdate("CREATE TABLE " + firstName + "_" + lastName + "("
+                        + "TIMESTAMP TIMESTAMP, "
+                        + "ACTION VARCHAR(256))");
+            }
+            
+            String insertQuery = "INSERT INTO " + firstName + "_" + lastName
+                    + " (TIMESTAMP, ACTION) VALUES (?, ?)";
+            PreparedStatement pstmt = conn.prepareStatement(insertQuery);
+            pstmt.setTimestamp(1, timeStamp);
+            pstmt.setString(2, action);
+            pstmt.executeUpdate();
+            
+//            // Test
+//            rs = stmt.executeQuery("SELECT * FROM " + firstName + "_" + lastName);
+//            while(rs.next())
+//            {
+//                System.out.println("Timestamp: " + rs.getTimestamp("TIMESTAMP") + "\n"
+//                        + "Action: " + rs.getString("ACTION") + "\n");  
+//            }
+//            System.out.println("");
+//            conn.close();
+        }
+        catch(Exception E)
+        {
+            E.printStackTrace();
+        }
+    }
+    
+//    public static void main(String[] args) 
+//    {
+//        dbGameLog("RAFAEL", "MARCO", "ENTERED FIRST NAME");
+//    }
+}
