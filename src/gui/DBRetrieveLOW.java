3783f749da3a229aeb10b105a6a571c11f9d3b46
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-10-17 19:48:17 +1300

    coded DBRetrieveLow with comments

--- src/gui/DBRetrieveLOW.java
+++ src/gui/DBRetrieveLOW.java
@@ -4,10 +4,53 @@
  */
 package gui;
 
+import java.util.ArrayList;
+import java.sql.*;
 /**
  *
  * @author rafae
  */
-public class DBRetrieveLOW {
-    
+
+/*
+    This class contains a method to retrieve all the list of past winners that has 
+    played Deal Or No Deal. It returns an ArrayList to be displayed in WinnersFrame
+*/
+public class DBRetrieveLOW 
+{
+    public ArrayList<String[]> retrieveWinnersList()
+    {
+        // ArrayList of list of winners
+        ArrayList<String[]> list = new ArrayList<>();
+        String dbURL = "jdbc:derby:dealornodealDB;create=true";
+        
+        try
+        {
+            // Establish connection
+            Connection conn = DriverManager.getConnection(dbURL);
+            Statement stmt = conn.createStatement();
+            
+            // Query to retrieve the data from table
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ListOfWinners");
+            
+            // Make entries to ArrayList containing String[] of data
+            while(rs.next())
+            {
+                String[] win = new String[4];
+                win[0] = rs.getDate("DATE_PLAYED").toString();
+                win[1] = rs.getString("FIRST_NAME");
+                win[2] = rs.getString("LAST_NAME");
+                win[3] = String.valueOf(rs.getDouble("AMOUNT_WON"));
+                
+                list.add(win);
+            }
+            conn.close();
+        }
+        catch(Exception E)
+        {
+            E.printStackTrace();
+        }
+        
+        // Return the list
+        return list;
+    }
 }
